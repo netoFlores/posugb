f46a619fbb703197e954557117d760cf8e79ffec
netoflores <ernesto.flores28@gmail.com>
2019-03-23 15:59:30 -0600

    Proyecto POS

---
+++ POS/Models/Producto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Producto
+    {
+        /*
+         Producto
+    tipoproducto
+    codigobar
+    codigoproducto
+    nombre
+    alto
+    ancho
+    descripcion
+    proveedor
+    marca
+         */
+         public String CODIGOBAR {get;set;}
+
+        public Int32 CODPRODUCTO { get; set; }
+    }
+}
---
+++ POS/POS/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Models;
+
+namespace POS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Producto producto = new Producto();
+            producto.CODPRODUCTO = Int32.Parse(textBox1.Text.Trim());
+            producto.CODIGOBAR = textBox2.Text;
+
+            label3.Text = "Codigo producto " + producto.CODPRODUCTO + "\n";
+            label3.Text += "Codigo de Barra " + producto.CODIGOBAR + "\n";
+
+        }
+    }
+}
